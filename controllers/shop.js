4f4cc3af2d9a2e666b7c761fe0b5c23fbd9fffae
mohit <rajpootmohit782@gmail.com>
2023-05-01 19:13:02 +0000

    Initial commit

---
+++ controllers/shop.js
@@ -0,0 +1,53 @@
+const Product = require("../models/product");
+
+exports.getProducts = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/product-list", {
+      prods: products,
+      pageTitle: "All Products",
+      path: "/products",
+    });
+  });
+};
+
+exports.getproduct = (req, res, next) => {
+  let prodid = req.params.productId;
+  Product.findMyId(prodid, (product) => {
+    console.log("prop", product);
+    res.render("shop/product-detail", {
+      product: product,
+      pageTitle: product.title,
+      path: "/product",
+    });
+  });
+};
+exports.getIndex = (req, res, next) => {
+  Product.fetchAll((products) => {
+    res.render("shop/index", {
+      prods: products,
+      pageTitle: "Shop",
+      path: "/",
+    });
+  });
+};
+
+exports.getCart = (req, res, next) => {
+  res.render("shop/cart", {
+    path: "/cart",
+    pageTitle: "Your Cart",
+  });
+};
+
+exports.getOrders = (req, res, next) => {
+  res.render("shop/orders", {
+    path: "/orders",
+    pageTitle: "Your Orders",
+  });
+};
+
+exports.getCheckout = (req, res, next) => {
+  res.render("shop/checkout", {
+    path: "/checkout",
+    pageTitle: "Checkout",
+  });
+};
